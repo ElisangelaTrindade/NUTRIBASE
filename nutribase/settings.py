9214c91def8361dddb7d1d82ebaf82faeca7c230
Elisangela Trindade <elisangelamaratrindade@gmail.com>
2021-09-21 18:43:19 +0100

    Renamed from myapplication to management

--- nutribase/settings.py
+++ nutribase/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #local apps
-    'myapplication.apps.MyapplicationConfig'
+    'management.apps.ManagementConfig'
 
     #3rd party
 ]
@@ -128,4 +128,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL ="myapplication.User"
+AUTH_USER_MODEL ="management.User"
