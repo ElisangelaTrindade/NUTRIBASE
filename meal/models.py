18e4966592e06b906aa9dc705d85cb6bad7ec72f
Elisangela Trindade <elisangelamaratrindade@gmail.com>
2021-10-06 16:53:08 +0100

    import model class from management to meal

--- meal/models.py
+++ meal/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from diet_plan.models import DietPlan
+from food_diary.models import FoodDiary
+from user.models import User
 
 # Create your models here.
 class Meal(models.Model):
