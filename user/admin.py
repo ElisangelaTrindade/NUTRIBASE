7d567bbed1efbbe7ffda3e8c11d854a878b3456c
Elisangela Trindade <elisangelamaratrindade@gmail.com>
2021-12-01 21:14:22 +0000

    Fix registered_by fk

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.contenttypes.admin import GenericTabularInline
+from django.contrib.contenttypes.models import ContentType
 from .models import User
 from phone.models import Phone
 from employee.models import Employee
@@ -21,11 +22,11 @@ class EmployeeInline(admin.StackedInline):
     min_num = 1
     can_delete= False
 
-def label_from_instance(self):
-    return self.employee
+    def label_from_instance(self):
+        return self.employee
   
-def __str__(self):
-    return self.employee
+    def __str__(self):
+        return self.employee
 
 
 @admin.register(User)
@@ -33,6 +34,20 @@ class UserAdmin(auth_admin.UserAdmin):
     inlines = (EmployeeInline, PhoneAdmin)
     search_fields = ['first_name','last_name','cpf', ]
 
+    def save_formset(self, request, form, formset, change):
+        for f in formset.forms:
+            obj = f.instance 
+            if hasattr(obj, 'registered_by_id'):
+                obj.registered_by_id = request.user.id
+            if hasattr(obj, 'content_type_id'):
+                obj.content_type_id = ContentType.objects.get_for_model(Phone).id
+            if hasattr(obj, 'object_id'):
+                obj.object_id = form.instance.id
+
+            obj.save()
+
+        formset.save()
+    
 
 
 
